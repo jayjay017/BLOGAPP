4ea31fcd3e6b1bebf0b38d016513b540069b01ce
jay <jayveiga12@hotmail.com>
2017-12-31 01:02:24 +0000

    add variable to grabbing posts from our database

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Createpost
 
 def list_detail (request):
-    return render (request, "blogapp/list_detail.html")
+    createposts = Createpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blogapp/list_detail.html', {'createposts': createposts})
+    
